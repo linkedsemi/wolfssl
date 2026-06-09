d4b4787ee57eca2be71b397f73678f35e668369b
Jc Shi <jcshi@linkedsemi.com>
2026-06-09 19:26:56 +0800

    wolfcrypt/src/port/linkedsemi/otbn_config.c : move the otbn configuration file to zephyr

--- wolfcrypt/src/port/linkedsemi/otbn_config.c
+++ wolfcrypt/src/port/linkedsemi/otbn_config.c
@@ -2,17 +2,17 @@
 #include "ls_msp_otbn.h"
 #include "reg_sysc_sec_cpu.h"
 #include "field_manipulate.h"
-#include "platform.h"
 #include "qsh.h"
 #include <wolfssl/wolfcrypt/port/linkedsemi/ls-otbn.h>
 #if defined(WOLFSSL_ZEPHYR)
 #include <zephyr/irq.h>
 #include <zephyr/kernel.h>
+#include "otbn/ls_otbn_config.h"
 #endif
 struct current_otbn otbn_info;
 
 #if defined(WOLFSSL_ZEPHYR)
-void wc_LS_OTBN_IRQHandler()
+void wc_LS_OTBN_IRQHandler(void *param)
 {
     if (LSOTBN->INTR_STATE)
     {
@@ -24,7 +24,7 @@ void wc_LS_OTBN_IRQHandler()
 #endif
 
 #if defined(WOLFSSL_ZEPHYR)
-void LS_OTBN_SYSC_IRQHandler(void);
+void ls_otbn_module_init(void);
 #endif
 void HAL_LSOTBN_MSP_Init(void);
 void HAL_LSOTBN_MSP_DeInit(void);
@@ -35,32 +35,8 @@ void wc_LS_Otbn_Module_Init(void)
 #endif
     // Supports both Zephyr and bare-metal operation
 #if defined(WOLFSSL_ZEPHYR)
-    uint32_t EDN_URND_BUS_IN = 0;
-    REG_FIELD_WR(SYSC_SEC_CPU->INTR_CTRL_INTR_MSK, SYSC_SEC_CPU_I_EDN_URND_REQ, 0);
-    SYSC_SEC_CPU->PD_CPU_CLKG[1] = SYSC_SEC_CPU_CLKG_CLR_OTBN_MASK;
-    SYSC_SEC_CPU->PD_CPU_SRST[1] = SYSC_SEC_CPU_SRST_CLR_OTBN_MASK;
-    SYSC_SEC_CPU->PD_CPU_SRST[1] = SYSC_SEC_CPU_SRST_SET_OTBN_MASK;
-    SYSC_SEC_CPU->PD_CPU_CLKG[1] = SYSC_SEC_CPU_CLKG_SET_OTBN_MASK;
-    for (uint8_t i = 0; i < 16; i++)
-    {
-        while (!REG_FIELD_RD(SYSC_SEC_CPU->OTBN_INTR_RAW, SYSC_SEC_CPU_I_EDN_URND_REQ)) ;
-        SYSC_SEC_CPU->EDN_URND_BUS = ++EDN_URND_BUS_IN;
-        REG_FIELD_WR(SYSC_SEC_CPU->OTBN_CTRL2, SYSC_SEC_CPU_EDN_URND_ACK, 1);
-        REG_FIELD_WR(SYSC_SEC_CPU->OTBN_CTRL2, SYSC_SEC_CPU_EDN_URND_ACK, 0);
-        SYSC_SEC_CPU->INTR_CLR_MSK = SYSC_SEC_CPU_I_EDN_URND_REQ_MASK;
-    }
-    SYSC_SEC_CPU->INTR_CLR_MSK = FIELD_BUILD(SYSC_SEC_CPU_I_EDN_RND_REQ, 1) |
-                            FIELD_BUILD(SYSC_SEC_CPU_I_EDN_URND_REQ, 1) |
-                            FIELD_BUILD(SYSC_SEC_CPU_I_OTBN_OTP_REQ, 1);
-    SYSC_SEC_CPU->INTR_CTRL_INTR_MSK = FIELD_BUILD(SYSC_SEC_CPU_I_EDN_RND_REQ, 1) |
-                              FIELD_BUILD(SYSC_SEC_CPU_I_EDN_URND_REQ, 1) |
-                              FIELD_BUILD(SYSC_SEC_CPU_I_OTBN_OTP_REQ, 1);
-
-
-    IRQ_CONNECT(OTBN_SYSC_IRQN, 3, LS_OTBN_SYSC_IRQHandler,NULL, 0);
-    irq_enable(OTBN_SYSC_IRQN);
-    IRQ_CONNECT(OBTN_IRQN, 3, wc_LS_OTBN_IRQHandler,NULL, 0);
-    irq_enable(OBTN_IRQN);
+    ls_otbn_done_callback_register(wc_LS_OTBN_IRQHandler, NULL);
+    ls_otbn_module_init();
 
     k_sem_init(&otbn_info.wait_complete,0,1);
 #else
